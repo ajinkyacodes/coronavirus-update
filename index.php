4bdeb52854e8954cf082f8380fa42435f43d0d36
Ajinkya G <ajinkyacodes@gmail.com>
2020-09-19 04:12:43 +0530

    Added Google fonts and styling modified

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Coronavirus Update</title>
 	<link rel="icon" href="favicon.gif" type="image/gif" sizes="16x16">
-	<link rel="stylesheet" media="screen" href="assets/vendor/bootstrap-4.3.1/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Mali:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200;1,300;1,400;1,500;1,600;1,700&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+	<link rel="stylesheet" media="screen" href="assets/vendor/bootstrap-4.3.1/bootstrap.min.css">	
 	<link rel="stylesheet" media="screen" href=<?php 
 	$cssfile = "assets/css/style.css";
 	$timestamp = "?".time();
